fb43913937b36f898bdaf1d06e26400aad968d55
Scott Levie <madeinscottland@gmail.com>
2019-04-08 10:23:08 -0600

    Tests: Added test for RecipedCalculatorPresenter.setBreadType

--- BreadCalculatorTests/TestRecipeCalculatorPresenter.swift
+++ BreadCalculatorTests/TestRecipeCalculatorPresenter.swift
@@ -28,29 +28,75 @@ class TestRecipeCalculatorPresenter: XCTestCase {
 
         self.testSubject.viewDidLoad()
 
+        print("------")
+        print("Test viewDidLoad:")
+
         // Assert setup methods are called exactly once
         guard self.assertCallCount(name: "setBreadType", count: self.view.calls.setBreadType.count, expectedCount: 1) else {
             return
         }
 
-        self.assertViewCallsForBreadType()
-    }
-
-    private func assertCallCount(name: String, count: Int, expectedCount: Int) -> Bool {
-        XCTAssert(count == expectedCount, "Called \(name) \(count) times. Expected \(expectedCount).")
-        return (count == expectedCount)
-    }
-
-    private func assertViewCallsForBreadType() {
-
-        guard let setBreadType = self.view.calls.setBreadType.first else {
+        print("  setBreadType was called exactly once")
+        guard let setBreadType = self.view.calls.setBreadType.last, (self.view.calls.setBreadType.count == 1) else {
             XCTFail()
             return
         }
 
-        // Assert animated is false
-        print("Test setBreadType.animated == false")
+        print("  (BreadType: \(setBreadType.breadType.debugName))")
+
+        print("  setBreadType was called WITHOUT animation")
         XCTAssertFalse(setBreadType.animated)
+
+        print("  endEditingField was NOT called")
+        XCTAssert(self.view.calls.endEditingField == 0)
+
+        print("------")
+    }
+
+    func testChangeBreadType() {
+
+        self.testSubject.viewDidLoad()
+
+        print("------")
+        print("Test viewDidChangeBreadType")
+
+        for breadType in BreadType.all {
+            let oldSetBreadTypeCount = self.view.calls.setBreadType.count
+            let oldEndEditingCount = self.view.calls.endEditingField
+
+            self.testSubject.viewDidChangeBreadType(breadType)
+
+            print("  Changed to \(breadType.debugName)")
+
+            print("    endEditingField was called exactly once")
+            let newEndEditingCount = self.view.calls.endEditingField
+            let endEditingDelta = (newEndEditingCount - oldEndEditingCount)
+            XCTAssert(endEditingDelta == 1, "Called endEditingField \(endEditingDelta) times")
+
+            print("    setBreadType was called exactly once")
+            let newSetBreadTypeCount = self.view.calls.setBreadType.count
+
+            guard (newSetBreadTypeCount == oldSetBreadTypeCount + 1) else {
+                XCTFail()
+                continue
+            }
+
+            let setBreadType = self.view.calls.setBreadType.last!
+
+            print("    setBreadType was called WITH animation")
+            XCTAssertTrue(setBreadType.animated)
+        }
+
+        print("------")
+    }
+
+
+    // MARK: - Assertions
+
+
+    private func assertCallCount(name: String, count: Int, expectedCount: Int) -> Bool {
+        XCTAssert(count == expectedCount, "Called \(name) \(count) times. Expected \(expectedCount).")
+        return (count == expectedCount)
     }
 }
 
